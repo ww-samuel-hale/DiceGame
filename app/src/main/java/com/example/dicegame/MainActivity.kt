7fd21a37081449e3a7ac26dad2051437a670ffc6
ww-samuel-hale <samuel.hale@woodwing.com>
2024-03-02 14:55:03 -0500

    Implemented diceroll

--- app/src/main/java/com/example/dicegame/MainActivity.kt
+++ app/src/main/java/com/example/dicegame/MainActivity.kt
@@ -19,6 +19,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import com.example.dicegame.R.drawable
+import kotlin.random.Random
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
 
 
 
@@ -39,10 +42,12 @@ fun MyScreenContent() {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
             Column(modifier = Modifier.padding(16.dp)) {
+                val diceRoll = remember { mutableStateOf(1) }
                 // Place your button inside this Column
                 Button(
                     onClick = {
                         // Handle the button click here
+                        diceRoll.value = Random.nextInt(1, 7)
                     },
                     modifier = Modifier.fillMaxWidth()
                 ) {
@@ -50,7 +55,14 @@ fun MyScreenContent() {
                 }
                 // ... other composables
                 Image (
-                    painter = painterResource(id = R.drawable.dice_1),
+                    painter = painterResource(id = when (diceRoll.value) {
+                        1 -> drawable.dice_1
+                        2 -> drawable.dice_2
+                        3 -> drawable.dice_3
+                        4 -> drawable.dice_4
+                        5 -> drawable.dice_5
+                        else -> drawable.dice_6
+                    }),
                     contentDescription = "Dice Image"
                 )
             }
